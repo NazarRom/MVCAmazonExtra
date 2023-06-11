a058a75aaeb246f6633e754635bef3d27b5e8809
NazarRom <nazarrom321@gmail.com>
2023-06-11 22:40:35 +0200

    commit

--- ExtraSliceV2/Helpers/HelperPdf.cs
+++ ExtraSliceV2/Helpers/HelperPdf.cs
@@ -19,8 +19,13 @@ namespace MVCAmazonExtra.Helpers
             string html = "<div style='font-family: Arial, sans-serif; background-color: #f9f9f9; margin: 0; padding: 20px;'><h1 style='text-align: center;'>Factura</h1><table style='width: 100%; border-collapse: collapse; margin-top: 20px;'><tr><th style='padding: 10px; text-align: left; background-color: #f2f2f2;'>Nombre</th><th style='padding: 10px; text-align: left; background-color: #f2f2f2;'>Cantidad</th><th style='padding: 10px; text-align: left; background-color: #f2f2f2;'>Precio</th><th style='padding: 10px; text-align: left; background-color: #f2f2f2;'>Total</th></tr><table style='width: 100%; border-collapse: collapse; margin-top: 20px;'>";
             double total = 0;
 
-            for (int i = 0; i < cantidad.Count; i++)
+            for (int i = 0; i < productos.Count; i++)
             {
+                html += "<tr><td style='padding: 10px;'>{productos[i].Nombre_producto}</td>" +
+                    $"<td style='padding: 10px;'>{cantidad[i]}</td>" +
+                    $"<td style='padding: 10px;'>{productos[i].Precio}€</td>" +
+                    $"<td style='padding: 10px;'>{productos[i].Precio * cantidad[i]}€</td>" +
+                    $"</tr>";
                 total += ((double)productos[i].Precio * cantidad[i]);
             }
             html += $"<td colspan='3' style='padding: 10px; text-align: right; font-weight: bold;'>Total:</td><td style='padding: 10px; font-size: 18px; color: #f44336;'>{total}€</td></tr></div>";
