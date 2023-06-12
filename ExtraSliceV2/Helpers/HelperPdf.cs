afc89c8c4691adab2cab023bc1575c3c407689f8
NazarRom <nazarrom321@gmail.com>
2023-06-12 10:28:35 +0200

    LETS GO BABY

--- ExtraSliceV2/Helpers/HelperPdf.cs
+++ ExtraSliceV2/Helpers/HelperPdf.cs
@@ -16,37 +16,38 @@ namespace MVCAmazonExtra.Helpers
             string basePath = Directory.GetCurrentDirectory();
             string ruta = Path.Combine(basePath, "wwwroot/", nombre);
             //string html = "<div><h1>Factura</h1><table><th>Nombre</th><th>Cantidad</th><th>Precio</th><th>Total</th></tr><table>";
-            string tablaHtml = "<table>";
-            tablaHtml += "<thead>";
-            tablaHtml += "<th>Producto</th>";
-            tablaHtml += "<th>Descripción</th>";
-            tablaHtml += "<th>Precio</th>";
-            tablaHtml += "<th>Cantidad</th>";
-            tablaHtml += "<th>Total</th>";
-            tablaHtml += "</thead>";
+            string tablaHtml = "<ul>";
+            //tablaHtml += "<thead>";
+            //tablaHtml += "<th>Producto</th>";
+            //tablaHtml += "<th>Descripción</th>";
+            //tablaHtml += "<th>Precio</th>";
+            //tablaHtml += "<th>Cantidad</th>";
+            //tablaHtml += "<th>Total</th>";
+            //tablaHtml += "</thead>";
             double total = 0;
             foreach (Producto pro in productos)
             {
                 total += (double)pro.Precio;
             }
-            tablaHtml += "<tbody>";
+            //tablaHtml += "<tbody>";
             for (var i = 0; i < productos.Count(); i++)
             {
                 Producto prod = productos[i];
                 int cant = cantidad[i];
 
-                tablaHtml += "<tr>";
-                tablaHtml += "<td>" + prod.Nombre_producto + "</td>";
-                tablaHtml += "<td>" + prod.Descripcion + "</td>";
-                tablaHtml += "<td>" + prod.Precio + "€" + "</td>";
-                tablaHtml += "<td>" + cant + "</td>";
+               // tablaHtml += "<tr>";
+                tablaHtml += "<li>" + prod.Nombre_producto + "/" + prod.Descripcion + "/"+ prod.Precio + "/" +cant+ "</li>";
+                //tablaHtml += "<td>" + prod.Descripcion + "</td>";
+                //tablaHtml += "<td>" + prod.Precio + "€" + "</td>";
+                //tablaHtml += "<td>" + cant + "</td>";
                 total += (double)prod.Precio * (cant - 1);
 
             }
-            tablaHtml += "<td>" + total + "€" + "</td>";
-            tablaHtml += "</tr>";
-            tablaHtml += "</tbody>";
-            tablaHtml += "</table>";
+            //tablaHtml += "<td>" + total + "€" + "</td>";
+            //tablaHtml += "</tr>";
+            //tablaHtml += "</tbody>";
+            tablaHtml += "</ul>";
+            tablaHtml += "<h3>TOTAL: " + total + "€" + "</h3>";
             //for (int i = 0; i < productos.Count; i++)
             //{
             //    //html += "<tr><td>{productos[i].Nombre_producto}</td>" +
